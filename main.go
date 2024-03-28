a711ee98b881d1b741e848396c22252a3ec0257a
subject026 <subject026@protonmail.com>
2024-03-28 13:01:07 +0000

    rm frontend url

--- main.go
+++ main.go
@@ -46,7 +46,7 @@ func main() {
 	router := chi.NewRouter()
 
 	router.Use(cors.Handler(cors.Options{
-		AllowedOrigins:   []string{"https://*", "http://*", "https://breadchain-governance.netlify.app"},
+		AllowedOrigins:   []string{"https://*", "http://*"},
 		AllowedMethods:   []string{"GET", "POST", "PUT", "DELETE", "OPTIONS"},
 		AllowedHeaders:   []string{"*"},
 		ExposedHeaders:   []string{"Link"},
